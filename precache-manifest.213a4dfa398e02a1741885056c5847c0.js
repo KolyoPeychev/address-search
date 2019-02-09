d58ca4636ea3c761010c85b1179833f28e9bfe8a
Kolyo Peychev <kolyo.peychev@yahoo.com>
2019-02-09 12:28:23 +0200

    Updates

---
+++ precache-manifest.213a4dfa398e02a1741885056c5847c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82600d46b8517e113a9a",
+    "url": "/address-search/static/js/runtime~main.82600d46.js"
+  },
+  {
+    "revision": "e1377b20c67058f94346",
+    "url": "/address-search/static/js/main.e1377b20.chunk.js"
+  },
+  {
+    "revision": "13dfb03664b8ea16d68f",
+    "url": "/address-search/static/js/1.13dfb036.chunk.js"
+  },
+  {
+    "revision": "e1377b20c67058f94346",
+    "url": "/address-search/static/css/main.dcf92307.chunk.css"
+  },
+  {
+    "revision": "13dfb03664b8ea16d68f",
+    "url": "/address-search/static/css/1.dc6d530a.chunk.css"
+  },
+  {
+    "revision": "64938c697f3a43d50256c6fc7829d5d7",
+    "url": "/address-search/index.html"
+  }
+];
